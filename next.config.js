9a268c3e2f97c03c4ef01ca7352c24c579f9dc3d
Makarand <madhavi.m@northeastern.edu>
2024-03-01 12:45:25 -0500

    add experimental server configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    experimental: {
+        serverActions: {
+          allowedOrigins: ['https://api-integration.makarandmadhavi.me'],
+        },
+    },
 };
 
 module.exports = nextConfig;
